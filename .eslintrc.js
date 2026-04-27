e5fb7612e8927cc71fba4c33b77b2cd2eb091914
Giorgi2201 <giorgiavgarozashvili@gmail.com>
2026-04-27 16:30:17 +0400

    chore(ios): plist, build scripts, Podfile, safe-area patch-package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  globals: {
+    console: 'readonly',
+    Promise: 'readonly',
+    setTimeout: 'readonly',
+    clearTimeout: 'readonly',
+    setInterval: 'readonly',
+    clearInterval: 'readonly',
+  },
 };
